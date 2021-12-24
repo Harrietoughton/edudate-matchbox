98028fdc4548ef78b9a483e326aeba0ee9207752
harrietoughton <harrietoughton@gmail.com>
2021-12-24 12:11:32 +0000

    Fix rubocop style issues

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,35 +12,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_23_100821) do
-
-  create_table "date_events", force: :cascade do |t|
-    t.datetime "time"
-    t.boolean "teacher_1_liked"
-    t.boolean "teacher_2_liked"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+ActiveRecord::Schema.define(version: 20_211_223_100_821) do
+  create_table 'date_events', force: :cascade do |t|
+    t.datetime 'time'
+    t.boolean 'teacher_1_liked'
+    t.boolean 'teacher_2_liked'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "edudates", force: :cascade do |t|
-    t.integer "teacher_id", null: false
-    t.integer "date_event_id", null: false
-    t.datetime "date"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["date_event_id"], name: "index_edudates_on_date_event_id"
-    t.index ["teacher_id"], name: "index_edudates_on_teacher_id"
+  create_table 'edudates', force: :cascade do |t|
+    t.integer 'teacher_id', null: false
+    t.integer 'date_event_id', null: false
+    t.datetime 'date'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['date_event_id'], name: 'index_edudates_on_date_event_id'
+    t.index ['teacher_id'], name: 'index_edudates_on_teacher_id'
   end
 
-  create_table "teachers", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "region"
-    t.string "interested_in"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'teachers', force: :cascade do |t|
+    t.string 'first_name'
+    t.string 'last_name'
+    t.string 'region'
+    t.string 'interested_in'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  add_foreign_key "edudates", "date_events"
-  add_foreign_key "edudates", "teachers"
+  add_foreign_key 'edudates', 'date_events'
+  add_foreign_key 'edudates', 'teachers'
 end
