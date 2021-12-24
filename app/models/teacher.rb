02c6881e2c775f048b85d72426970db276078130
harrietoughton <harrietoughton@gmail.com>
2021-12-24 16:55:53 +0000

    Add gender and age to teacher model

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -3,4 +3,6 @@
 class Teacher < ApplicationRecord
   has_many :edudates
   has_many :date_events, through: :edudates
+  as_enum :gender, %i[male female]
+  as_enum :age, %i[under_thirty_nine forty_plus]
 end
