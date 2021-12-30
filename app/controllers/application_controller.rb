9f459bd82aeeaba59ceee557267ee4734dadaaef
harrietoughton <harrietoughton@gmail.com>
2021-12-30 18:00:05 +0000

    EDU-1 Set up application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # skip CSRF token match check because we have an API here - it's already taken care of
+  skip_before_action :verify_authenticity_token
 end
