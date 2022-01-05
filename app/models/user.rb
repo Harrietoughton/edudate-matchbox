4ad8d2bcc7619b08c6995af7cc974b811eab88d7
harrietoughton <harrietoughton@gmail.com>
2022-01-05 16:01:50 +0000

    EDU-1 Create sign_up with :username and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,18 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :authentication_keys => [:username]
   # tells the user model that the password_digest field needs to be encyrpted when stored in the DB
   # has_secure_password
 
   validates_presence_of :username
   validates_uniqueness_of :username
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
 end
